adb2d600f158e516f6775f31997602d0367baf3f
Amit Soni <amit.soni5157@gmail.com>
2026-06-28 12:14:48 +0530

    main file and changed and gorq added in requirements

---
+++ main.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
+
+# 1. Schema class definition ko top level par rakha
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+# Baaki ka code...
+def main():
+    # 2. load_dotenv() ko sabse pehle call kiya taaki API Key active ho jaye
+    load_dotenv()
+    
+    # llm = ChatOpenAI(model="gpt-4o0-mini")
+    llm = ChatGroq(model="llama-3.3-70b-versatile")
+    # response = llm.invoke("What is the meaning of life ?")
+    parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """ 
+                "You are a research assistant that will help generate a research paper."
+                Answer the user query and use necessary tools.
+                wrap the output in this format and provide no other text\n{format_instructions}
+                """,
+            ),
+            ("placeholder", "{chat_history}"),
+            ("human", "{query}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    ).partial(format_instructions=parser.get_format_instructions())
+
+    agent = create_tool_calling_agent(
+        llm=llm,
+        prompt=prompt,
+        tools=[]
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+    raw_response = agent_executor.invoke({"query": "What is the capital of France ?"})
+    print(raw_response) 
+
+    try:
+        structured_response = parser.parse(raw_response.get("output",[0]["text"]))
+    except Exception as e:
+        print(f"Error parsing response ", e,"Raw response:",raw_response)
+
+if __name__ == '__main__':
+    main()
